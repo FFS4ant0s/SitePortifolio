382d079532d6f5d7f07d932c5fcc5c6759e48705
Fernando <silentguyband@gmail.com>
2025-02-26 14:06:44 -0300

    Arrumando o ABOUT.js

--- Portifolio_Front_end/src/components/About.js
+++ Portifolio_Front_end/src/components/About.js
@@ -3,10 +3,12 @@ import minhaFoto from '../assets/foto_minha.png';
 
 function About() {
   return (
-    <section id="about" class='hero'>
-      <img src={minhaFoto} alt="Minha Foto" />
-      <h2>Sobre Mim</h2>
-      <p>Sou Fernando, um desenvolvedor fullstack apaixonado por tecnologia...</p>
+    <section id="about" className="hero">
+      <img src={minhaFoto} alt="Minha Foto" className="profile-photo" />
+      <div className="hero-content">
+        <h2>Sobre Mim</h2>
+        <p>Sou Fernando, um desenvolvedor fullstack apaixonado por tecnologia...</p>
+      </div>
     </section>
   );
 }
