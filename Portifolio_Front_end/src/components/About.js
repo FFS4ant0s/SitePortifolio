ade3878e81dfe9bbcb1058f9f5a35649d75d4b76
Fernando <silentguyband@gmail.com>
2025-02-26 15:32:29 -0300

    Description with my

--- Portifolio_Front_end/src/components/About.js
+++ Portifolio_Front_end/src/components/About.js
@@ -7,7 +7,11 @@ function About() {
       <img src={minhaFoto} alt="Minha Foto" className="profile-photo" />
       <div className="hero-content">
         <h2>Sobre Mim</h2>
-        <p>Sou Fernando, um desenvolvedor fullstack apaixonado por tecnologia...</p>
+        <p>Olá, Sou um programador fullstack com experiência em
+          desenvolvimento de soluções web. Meu foco abrange tanto o
+          front-end quanto o back-end. Tenho habilidades em arquitetura
+          de software, integração de APIs e otimização de desempenho.
+          Estou sempre em busca de novos desafios e oportunidades.</p>
       </div>
     </section>
   );
