3e68b634bb574acdf7935a724b9674fd2070d0c4
Fernando <silentguyband@gmail.com>
2025-03-27 13:34:03 -0300

    Mudando contato

--- Portifolio_Front_end/src/components/Contact.js
+++ Portifolio_Front_end/src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     <h2>Contato</h2>
     <p>
       Me envie uma mensagem pelo 
-      <a href="https://wa.me/5561986115281" target="_blank" rel="noopener noreferrer"> WhatsApp</a>
+      <a href="https://wa.me/5561991680627" target="_blank" rel="noopener noreferrer"> WhatsApp</a>
       <br /> ou pelo e-mail 
       <a href="mailto:ff.s4ntoss@gmail.com"> ff.s4ntoss@gmail.com</a>
     </p>
