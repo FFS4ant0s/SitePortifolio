1d88a68c642c35a714c2ea4ccb1a21ae50bc8c9d
Fernando <silentguyband@gmail.com>
2025-04-07 16:24:56 -0300

    Ultimos ajustes do deploy

--- Portifolio_Back_end/portfolio/urls.py
+++ Portifolio_Back_end/portfolio/urls.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from projects.views import ProjectViewSet
@@ -8,7 +9,13 @@ from django.conf import settings
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
 
+
+def home(request):
+    return JsonResponse({"status": "API Online", "message": "Portfólio backend rodando"})
+
+
 urlpatterns = [
+    path('', home),
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
