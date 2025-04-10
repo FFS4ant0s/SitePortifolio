40b89126ea7e99a58e628a71c20c4c7faf893223
Fernando <silentguyband@gmail.com>
2025-04-10 13:40:15 -0300

    Adicionando imagens da pasta media

--- Portifolio_Back_end/portfolio/settings.py
+++ Portifolio_Back_end/portfolio/settings.py
@@ -114,7 +114,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Arquivos de mídia
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Campo padrão para primary key
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
